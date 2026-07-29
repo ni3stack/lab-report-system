ac8fdbaaa85d5d0f7f05c65b8844c6d297e59aad
Nitin Pathak <nitinpathak211@gmail.com>
2026-07-29 15:03:34 +0530

    feat: add reusable stats card component (#3)

---
+++ src/ui/components/StatsCard.js
@@ -0,0 +1,18 @@
+/**
+ *  Renders a stats card component with a title and value.
+ * 
+ * @param {Object} props 
+ * @param {string} props.title 
+ * @param {number|string} props.value 
+ * @returns {string} HTML string representing the stats card component.
+ * 
+ */
+
+export function StatsCard({ title, value}) {
+    return `
+        <div class="stats-card">
+            <div class="stats-title">${title}</div>
+            <div class="stats-value">${value}</div>
+        </div>
+    `;
+}
