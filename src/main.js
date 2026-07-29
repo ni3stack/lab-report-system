f82682b295d40ce1490fb08f03631d5b5bbe9ab7
Nitin Pathak <nitinpathak211@gmail.com>
2026-07-29 14:46:34 +0530

    chore: initialize Vite vanilla JavaScript project (#1)

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import "./css/main.css";
+import "./css/dashboard.css";
+import "./css/sidebar.css";
+import "./css/components.css";
+import "./css/components/table.css";
+
+import { init } from "./app";
+
+init();
