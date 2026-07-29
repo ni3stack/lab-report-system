3fb0c1dc0b85199ee719fdc7acd7e40e58724925
Nitin Pathak <nitinpathak211@gmail.com>
2026-07-29 14:58:14 +0530

    feat: add reusable stats card component

---
+++ src/data/dashboardStats.js
@@ -0,0 +1,6 @@
+export const dashboardStats = [
+    { title: "Total Reports", value: 154 },
+    { title: "Todays Reports", value: 12 },
+    { title: "Pending", value: 3 },
+    { title: "Patients", value: 86 }
+];
